d6ea837b6a25697bcbabdaeaed95e62ab1020128
Mohammad <Mohammad@DESKTOP-T8KURCL>
2022-10-05 21:58:27 +0300

    comms

---
+++ test123/src/test/java/Travel2022_Tests/LoginTests.java
@@ -0,0 +1,28 @@
+package Travel2022_Tests;
+
+import static org.junit.Assert.assertTrue;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginTests {
+
+
+	@Given("that the user inters the right UserName and Password")
+	public void thatTheUserIntersTheRightUserNameAndPassword() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the user gets  to the main Menu")
+	public void theUserGetsToTheMainMenu() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+
+
+
+}
---
+++ test123/test_cases/Log.feature
@@ -0,0 +1,7 @@
+Feature: Login 
+
+Scenario: User attemps to login with right UserName and Password
+Given that the user inters the right UserName and Password
+
+Then the user gets  to the main Menu
+ 
