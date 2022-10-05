d01b729cebc5dbd5fbe38d72585182ee6337bc1f
Mohammad <Mohammad@DESKTOP-T8KURCL>
2022-10-05 18:28:19 +0300

    commite

---
+++ travel2022Project/src/main/java/travel2022Project/customer.java
@@ -0,0 +1,10 @@
+package travel2022Project;
+
+public class customer {
+
+	public int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+}
---
+++ travel2022Project/src/test/java/travel2022Project/LoginSteps.java
@@ -0,0 +1,52 @@
+package travel2022Project;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+
+	@Given("that the user the right UserName and Password")
+	public void thatTheUserTheRightUserNameAndPassword(String Username,String Password) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the UserName and Password gets Validated")
+	public void theUserNameAndPasswordGetsValidated() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the user gets  to the main Menu")
+	public void theUserGetsToTheMainMenu() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+	@Given("that the user desent provides the right UserName and Password")
+	public void thatTheUserDesentProvidesTheRightUserNameAndPassword() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the UserName and Password gets Validated")
+	public void theUserNameAndPasswordGetsValidatedAgain() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the user gets a wrong UserName or Password Message")
+	public void theUserGetsAWrongUserNameOrPasswordMessage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("has to try to login again")
+	public void hasToTryToLoginAgain() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+
+}
