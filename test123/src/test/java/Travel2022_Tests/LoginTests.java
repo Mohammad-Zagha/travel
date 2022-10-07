b65b6623f7b3b999833d16a7650aa714073bb5ce
Mohammad <Mohammad@DESKTOP-T8KURCL>
2022-10-07 15:08:46 +0300

    Edit on the login feature

--- test123/src/test/java/Travel2022_Tests/LoginTests.java
+++ test123/src/test/java/Travel2022_Tests/LoginTests.java
@@ -9,16 +9,38 @@ import io.cucumber.java.en.When;
 public class LoginTests {
 
 
-	@Given("that the user inters the right UserName and Password")
-	public void thatTheUserIntersTheRightUserNameAndPassword() {
+
+
+
+	@Given("the users in the system are")
+	public void theUsersInTheSystemAre(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("that the user enters the right UserName and Password")
+	public void thatTheUserEntersTheRightUserNameAndPassword() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
-	@Then("the user gets  to the main Menu")
+	@When("user the user calles the login function")
+	public void userTheUserCallesTheLoginFunction() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the user gets to the main Menu")
 	public void theUserGetsToTheMainMenu() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+
+
+
 
 
 
--- test123/test_cases/Log.feature
+++ test123/test_cases/Log.feature
@@ -1,7 +1,11 @@
 Feature: Login 
 
-Scenario: User attemps to login with right UserName and Password
-Given that the user inters the right UserName and Password
+Background: the system has the following users 
+Given the users in the system are
+				|Mohammad|Pass123|
 
-Then the user gets  to the main Menu
+Scenario: User attemps to login with right UserName and Password
+Given that the user enters the right UserName and Password
+When user the user calles the login function
+Then the user gets to the main Menu
  
