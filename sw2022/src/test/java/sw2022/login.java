68a95afba05f4cf5f043229d0ee54d498ed5eede
Rosol Tuhul <Rosol Tuhul@DESKTOP-NQ9C8DP>
2022-10-09 19:25:34 +0300

    login commit!

--- sw2022/src/test/java/sw2022/login.java
+++ sw2022/src/test/java/sw2022/login.java
@@ -5,76 +5,25 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class login {
-	User user;
-	String username;
-	String password;
-	public login()
-	{
-		 user= new User("test123","usertest");
+	@Given("User is on registration page")
+	public void user_is_on_registration_page() {
+	System.out.println("user navigates on registration page");
 	}
-	
+@When("user enters following user details")
+public void user_enters_following_user_details(DataTable dataTable) {
+	List<List<String>> userList=dataTable.asLists(String.class);
+for(List<String> e: userList)
+{System.out.println(e);}
+}
 
-	@Given("that the User is not logged in")
-	public void thatTheUserIsNotLoggedIn() {
-	    user.Logout();
-	  
-	}
-
-
-
-	@Given("the password is {string} the username is {string}")
-	public void thePasswordIsTheUsernameIs(String string, String string2) {
-		this.password="usertest";
-		this.username="test123";
-	}
-
-
-	@Then("the user login succeeds")
-	public void theUserLoginSucceeds() {
-		assertEquals( user.LoggedIn(username,password) , true );
-	}
-	@Then("the user is logged in")
-	public void theUserIsLoggedIn() {
-		assertEquals(user.LogInStatus,true);
-		
-	}
-
-
-
-
-	@Given("that the user is not logged in")
-	public void thatTheuserIsNotLoggedIn() {
-		   user.Logout();
-	}
-
-
-
-
-	@Given("the password is {string} and\\/or username is {string}")
-	public void thePasswordIsAndOrUsernameIs(String string, String string2) {
-		this.password="test";
-		this.username="user";
-	}
-	@Then("the user login fails")
-	public void theUserLoginFails() {
-		assertEquals( user.LoggedIn(username,password) , false );
-		
-	}
-	@Then("the user is not logged in")
-	public void theUserIsNotLoggedIn() {
-		assertEquals(user.LogInStatus,false);
-	}
-
-
-
-
-
-
+@Then("user registration sould be successful")
+public void user_registration_should_be_successful() {}
 
 }
--- sw2022/use_case/login.feature
+++ sw2022/use_case/login.feature
@@ -1,15 +1,10 @@
-Feature: User login
-	Description: The User logs into the System
-	
-	
-Scenario: User can login
-	Given that the User is not logged in
-	And the password is "test123" the username is "usertest"
-	Then the user login succeeds
-	And the user is logged in
-	
-Scenario: user has the wrong password or username
-	Given that the user is not logged in
-	And the password is "test" and/or username is "user"
-	Then the user login fails
-	And the user is not logged in
+Feature: User Registration
+Scenario: User registratioin with differnet data
+Given User is on registration page
+When User enters following user details
+| rosol | tuhul | rosol@gmail.com |999 | Nablus |
+| mohammad | zagha | mhm@gmail.com |939 | Nablus |
+| amer | zagha | amer@gmail.com |959 | Nablus |
+Then user registration should be successful
+
+
