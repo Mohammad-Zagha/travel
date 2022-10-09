5ffc35eb1bf76a1744f1e3ab36fc7129f63247e6
Rosol Tuhul <Rosol Tuhul@DESKTOP-NQ9C8DP>
2022-10-09 20:53:53 +0300

    no error RUNtest successful

--- sw2022/src/test/java/sw2022/login.java
+++ sw2022/src/test/java/sw2022/login.java
@@ -11,17 +11,21 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 
-public class login {
-	@Given("User is on registration page")
+public class login 
+{
+@Given("User is on registration page")
 	public void user_is_on_registration_page() {
 	System.out.println("user navigates on registration page");
 	}
-@When("user enters following user details")
-public void user_enters_following_user_details(DataTable dataTable)
+@When("User enters following user details")
+public void User_enters_following_user_details(DataTable dataTable)
 {
 	List<List<String>> userList=dataTable.asLists(String.class);
-for(List<String> e: userList)
-{System.out.println(e);}
+	
+    for(List<String> e: userList)
+    { 
+    	System.out.println(e);
+    }
 }
 
 @Then("user registration should be successful")
