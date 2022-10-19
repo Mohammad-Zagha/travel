7ebd8429015810fd5b9ac4ed58ac95ff48fb3edb
Rosol Tuhul <Rosol Tuhul@DESKTOP-NQ9C8DP>
2022-10-19 14:33:31 +0300

    Admin Class

--- sw2022/src/main/java/sw2022/admin.java
+++ sw2022/src/main/java/sw2022/admin.java
@@ -1,9 +1,33 @@
 package sw2022;
 
 public class admin {
+	
 
-	public admin() {
-		// TODO Auto-generated constructor stub
+	protected String name;
+	protected String password;
+	protected boolean isLogged;
+	
+	public admin(String name, String password) {
+		//super(UserName, PassWord);
+		this.name = name;
+		this.password = password;
+		this.isLogged = false;
 	}
+	
+	
+	public boolean login(String password)
+	{this.isLogged = this.password.equals(password);
+	return this.isLogged;
+	}
+	
+    public boolean logout()
+	{this.isLogged = false;
+	return this.isLogged;
+	}
+	
+	public boolean isLogged()
+	{return this.isLogged;
+	}
+	
 
 }
